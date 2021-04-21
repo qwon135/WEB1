03783bad8b260b3746607a1488fe0c4d0a102136
qwon135 <82475863+qwon135@users.noreply.github.com>
2021-04-21 18:40:26 +0900

    Add files via upload

---
+++ color.js
@@ -0,0 +1,32 @@
+var Body = {
+  setColor:function(color){
+    document.querySelector('body').style.color=color;
+  },
+  setBackgroundColor:function(color){
+    document.querySelector('body').style.backgroundColor=color;
+  }
+}
+var Links={
+  setColor:function(color){
+      var alist=document.querySelectorAll('a');
+      var i=0;
+      while(i<alist.length){
+          alist[i].style.color=color;
+          i=i+1;
+    }
+  }
+}
+function nightDayHandler(self){
+  var target=document.querySelector('body');
+  if(self.value==='night'){
+    Body.setColor('white');
+    Body.setBackgroundColor('black');
+    self.value='day';
+    Links.setColor('white');
+  }else{
+    Body.setColor('black');
+    Body.setBackgroundColor('white');
+    self.value='night';
+    Links.setColor('black')
+    }
+  }
